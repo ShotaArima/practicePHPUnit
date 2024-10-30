a6b8cebb1ef4066158a0274df5339b3f74631754
ShotaArima <shota.arima@prtimes.co.jp>
2024-10-30 12:37:06 +0900

    update src and teats

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\Phpunit;
 
-class Calculator {
-	public function add(int $FirstPrice, int $SecondPrice): int {
-		return $FirstPrice + $SecondPrice;
-	}
+class Calculator
+{
+    public function add(int $firstPrice, int $SecondPrice): int
+    {
+        return $firstPrice + $SecondPrice;
+    }
 
-	public function sub(int $FristPrice, int $SecondPrice): int {
-		return $FirstPrice - $SecondPrice;
-	}
-
+    public function sub(int $firstPrice, int $SecondPrice): int
+    {
+        return $firstPrice - $SecondPrice;
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,15 +2,19 @@
 
 namespace App\Phpunit;
 
-use PHPUnit\Framework\TestCode;
+use PHPUnit\Framework\TestCase;
 
-class CalculatorTest extends TestCode {
-    public function test_合計金額() {
+class CalculatorTest extends TestCase
+{
+    public function test_合計金額()
+    {
         $calculator = new Calculator();
-        $this->asserrSame(12, $calculator->add(10,2));
+        $this->assertSame(12, $calculator->add(10, 2));
     }
-    public function test最初の金額から次の金額を引いた金額() {
+
+    public function test最初の金額から次の金額を引いた金額()
+    {
         $calculator = new Calculator();
-        $this->asserrSame(8, $calculator->sub(10,2));
+        $this->assertSame(8, $calculator->sub(10, 2));
     }
 }
